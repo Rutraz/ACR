ef9d8f17ef7a48ee7ebe2f298543142721ef0189
Rutraz <rutraavs@gmail.com>
2019-11-26 01:32:29 +0000

    Profile-Scroll

--- Clinic/resources/views/Client/profile.blade.php
+++ Clinic/resources/views/Client/profile.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.client')
 
 @section('content')
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<script src="{{ asset('js/jquery.fullpage.js')}}" ></script>
+
+
+
 <div class="profile">
-    <section class="intro">
+<div id="fullpage">
+    <section>
         <div class="client-info">
             <div class="profile-name">
                 <h1>{{$user->name}}</h1>
@@ -37,66 +43,88 @@
     <div class="client-appointmments">
                 <div class="appointmments-name"> 
                     <h1>Consultas</h1>
-                    <p>Actualize as informações do seu perfil e as definições.</p>
+                    <p>Consulte as suas Consultas.</p>
                     <hr>
                 </div>
                 <br>
                 <div  class="appointmments-table">
-                    
-                        <div id="scrolltable">
-                            <table>
-                               
-                                <tr >
+                            <table class="table-container">
+                               <thead>
+                                    <tr >
+                                         <th > Data </th> 
+                                         <th > Especialização </th> 
+                                         <th > Médico </th> 
+                                         <th > Observação </th> 
+                                         <th > Rating </th>
+                                         <th > State </th>
+                                    </tr>                              
+                               </thead>
+                                <tbody>
+
                                 
-                                <div> <th> Data </th> </div>
-                                <div> <th> Especialização </th> </div>
-                                <div> <th> Médico </th> </div>
-                                <div> <th> Observação </th> </div>
-                                <div> <th> Estado</th> </div>
-                
-                                </tr>
                                     @if ($appointments->isNotEmpty())
                                         @foreach($appointments as $appointment) 
                                             <tr>
                                                 <td>{{$appointment->date}}</td>
                                                 <td>{{$appointment->medic->specialty}}</td>
                                                 <td>{{$appointment->medic->user->name}}</td>
-                                                <td>{{$appointment->comments}}</td>
-                                                <td>{{$appointment->state}}</td>                        
+                                                <td>{{$appointment->Comentario}}</td>
+                                                <td>{{$appointment->rating}}</td>
+                                                <td>{{$appointment->state}}</td>                            
                                             </tr>
+                                           
                                                                     
                                         @endforeach
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
+                                        <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>
                                     @else
                                         <h1> Nao tem consultas marcadas ou realizadas </h1>
-                                    @endif
-                                    <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>  
-                                    <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>  
-                                    <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>  
-                                    <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr> 
-                                    <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>  
-                                    <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>  
-                                     <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr> 
-                                     <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>  
-                                     <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>  
-                                     <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>  
-                                     <tr><td>test</td><td>test</td><td>test</td><td>test</td><td>test</td></tr>   
-                            </table>
-                        </div>
-                     
-                </div>
-    </div>
+                                    @endif  
+                                     
+                               
+                                </tbody>   
+                             </table>
+                </div>           
+        </div>
                 
     </section>
     <section>
-    <h1>Analises</h1>
-    @if ($analysis->isNotEmpty())
-        @foreach($analysis as $analyse) 
-                <h1>{{$analyse->date}}</h1> 
-                <h2>{{$analyse->state}}</h2> 
-            @endforeach
-    @else
-        <h1> Nao tem analises marcadas ou realizadas </h1>
-    @endif
+        <div class="client-analysis">
+            <div class="anaylsis-name">
+                <h1>Analises</h1>
+                <p>Consulte as suas Analises</p>
+                <hr>
+            </div>
+        </div>
+                    
+                    @if ($analysis->isNotEmpty())
+                        @foreach($analysis as $analyse) 
+                                <h1>{{$analyse->date}}</h1> 
+                                <h2>{{$analyse->state}}</h2> 
+                            @endforeach
+                    @else
+                        <h1> Nao tem analises marcadas ou realizadas </h1>
+                    @endif
     </section>
+</div>
+
+<script > 
+
+  $('#fullpage').fullpage();
+
+</script>
+
+
 </div>
 @endsection
